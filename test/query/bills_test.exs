f8bc68a5bcc6f2e9e497d00a2a761e27ac41664e
Kevin Deisz <kevin.deisz@gmail.com>
2017-02-15 23:38:29 -0500

    Bills tests

---
+++ test/query/bills_test.exs
@@ -0,0 +1,30 @@
+defmodule ProPublica.Query.BillsTest do
+  use ExUnit.Case
+
+  @client ProPublica.Client.new(System.get_env("TOKEN"))
+
+  test "#recent introduced in the senate" do
+    %{"results" => [%{"bills" => _bills}]} =
+      @client |> ProPublica.Query.Bills.recent("senate", "introduced", 115)
+  end
+
+  test "#recent_by introduced by member C001098" do
+    %{"results" => [%{"bills" => _bills}]} =
+      @client |> ProPublica.Query.Bills.recent_by("C001098", "introduced")
+  end
+
+  test "#show bill s375" do
+    %{"results" => [%{"actions" => _actions}]} =
+      @client |> ProPublica.Query.Bills.show("s375", 115)
+  end
+
+  test "#show_metadata bill s375 amendments" do
+    %{"results" => [%{"amendments" => _amendments}]} =
+      @client |> ProPublica.Query.Bills.show_metadata("s375", "amendments", 115)
+  end
+
+  test "#cosponsors bill s375" do
+    %{"results" => [%{"cosponsors" => _cosponsors}]} =
+      @client |> ProPublica.Query.Bills.cosponsors("s375", 115)
+  end
+end
