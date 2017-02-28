bf47839a3a8f33bdc61d2f5f84dc2bfed402fdbc
Kevin Deisz <kevin.deisz@gmail.com>
2017-02-27 21:07:03 -0500

    Committees and tests

--- lib/pro_publica/query/committees.ex
+++ lib/pro_publica/query/committees.ex
@@ -1,19 +1,19 @@
 defmodule ProPublica.Query.Committees do
   import ProPublica
 
-  @spec membership(Client.t, binary, integer) :: ProPublica.response
-  def membership(client, chamber, congress \\ ProPublica.congress)
-  def membership(client, chamber, congress) when chamber in ["house", "senate"] do
+  @spec list(Client.t, binary, integer) :: ProPublica.response
+  def list(client, chamber, congress \\ ProPublica.congress)
+  def list(client, chamber, congress) when chamber in ["house", "senate"] do
     client |> get("/#{congress}/#{chamber}/committees.json")
   end
-  def membership(_, _, _), do: invalid_chamber()
+  def list(_, _, _), do: invalid_chamber()
 
-  @spec membership_for(Client.t, binary, integer, integer) :: ProPublica.response
-  def membership_for(client, chamber, committee_id, congress \\ ProPublica.congress)
-  def membership_for(client, chamber, committee_id, congress) when chamber in ["house", "senate"] do
+  @spec show(Client.t, binary, integer, integer) :: ProPublica.response
+  def show(client, chamber, committee_id, congress \\ ProPublica.congress)
+  def show(client, chamber, committee_id, congress) when chamber in ["house", "senate"] do
     client |> get("/#{congress}/#{chamber}/committees/#{committee_id}.json")
   end
-  def membership_for(_, _, _, _), do: invalid_chamber()
+  def show(_, _, _, _), do: invalid_chamber()
 
   @spec invalid_chamber :: no_return
   defp invalid_chamber, do: raise ArgumentError, message: "chamber must be either house or senate"
---
+++ test/query/committees_test.exs
@@ -0,0 +1,15 @@
+defmodule ProPublica.Query.CommitteesTest do
+  use ExUnit.Case
+
+  @client ProPublica.Client.new(System.get_env("TOKEN"))
+
+  test "#list committees in the senate" do
+    %{"results" => [%{"chamber" => "Senate", "committees" => _committees }]} =
+      @client |> ProPublica.Query.Committees.list("senate", 115)
+  end
+
+  test "#show a specific committee in the senate" do
+    %{"results" => [%{"chamber" => "Senate", "current_members" => _current_members }]} =
+      @client |> ProPublica.Query.Committees.show("senate", "SSAF", 115)
+  end
+end
